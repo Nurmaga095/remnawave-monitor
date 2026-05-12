b0aa8b9ea426b46bb1e6789f8753847dae24d902
Remnawave Monitor <you@example.com>
2026-05-13 00:43:19 +0300

    fix: userKey -> key in drop-reconnect button onclick

--- public/js/app.js
+++ public/js/app.js
@@ -3674,7 +3674,7 @@ function userCardHtml(u) {
                     ${IC._s('<path d="M1 4v6h6"/><path d="M3.51 15a9 9 0 1 0 2.13-9.36L1 10"/>', 14)}
                     Получить IP
                   </button>
-                  <button class="btn-mini btn-warn" onclick="startDropReconnect('${escAttr(userKey)}', '${escAttr(uuid)}')" id="drop-reconnect-btn" title="Сбросить соединения и проверить сколько IP переподключится">
+                  <button class="btn-mini btn-warn" onclick="startDropReconnect('${escAttr(key)}', '${escAttr(uuid)}')" id="drop-reconnect-btn" title="Сбросить соединения и проверить сколько IP переподключится">
                     ${IC._s('<path d="M9.88 9.88a3 3 0 1 0 4.24 4.24"/><path d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"/><path d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"/><line x1="2" y1="2" x2="22" y2="22"/>', 14)}
                     Проверить шаринг
                   </button>
